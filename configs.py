2f50087dc8b24926ab97de7d5f28bb0071125000
Mrblackgodd <136906179+Mrblackgodd@users.noreply.github.com>
2024-01-25 20:50:41 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,12 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
-    CHID = int(getenv("CHID", "-1000112234"))
+    API_ID = int(getenv("API_ID", "25377875"))
+    API_HASH = getenv("API_HASH", "cf80e342be48570ca2e4c9d2c7695413")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6435555746:AAH38KE9X7FthT4bD7hcdGRNxUVgV5mKx5g")
+    CHID = int(getenv("CHID", "-1001948610504"))
     SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
-    LOGCHID = int(getenv("LOGCHID", "-1000112234"))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://nehal969797:nehalsingh969797@cluster0.4xsgmgm.mongodb.net/?retryWrites=true&w=majority")
+    LOGCHID = int(getenv("LOGCHID", "-1001748572062"))
     API = getenv("API", "abcdefu67-8dgdg")
 cfg = Config()
